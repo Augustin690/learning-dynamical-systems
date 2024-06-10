9e56bcfe05a4350e24dda8bb89235322d3260eee
Augustin <augustin.curinier@laposte.net>
2024-06-10 23:14:01 +0200

    task 1 mostly completed

--- src/part1/utils.py
+++ src/part1/utils.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from scipy.linalg import lstsq
 import pandas as pd
 from scipy.spatial.distance import cdist
+from scipy.interpolate import RBFInterpolator
 
-# TODO: Define all the functions (in addition to the ones below) required to complete tasks 1-3 in this python file. 
+
+# TODO: Define all the functions (in addition to the ones below) required to complete tasks 1-3 in this python file.
 
 # Evaluation of a single Radial Basis Function (RBF)
 def radial_basis_function(x, x_c, eps):
@@ -19,21 +21,51 @@ def radial_basis_function(x, x_c, eps):
     Returns:
     - Radial basis function values evaluated at data points x
     """
-    # Hint: Use cdist from scipy.spatial.distance 
-    pass
+    # Hint: Use cdist from scipy.spatial.distanc
+    phi_x = np.exp(-(cdist(x_c, x, 'sqeuclid') / eps))
+
+    return phi_x
 
 
-def least_squares(A, b, cond=1e-5):
+def diameter(X):
+    return np.max(X) - np.min(X)
+
+
+def approx_non_linear_function(X, Y, L, ratio):
+    x_c = np.linspace(np.min(X), np.max(Y), L).reshape((L, 1))
+    X = X.reshape((X.shape[0], 1))
+    eps = ratio * diameter(X)
+    phi_x = radial_basis_function(X, x_c, eps)
+    # return transform(X, least_squares(phi_x, Y))
+    return phi_x
+
+
+def built_int_interpolator(X, Y, eps):
+    #X_augment = np.column_stack([X, np.ones(X.shape)])
+
+    return RBFInterpolator(X, Y, kernel='gaussian', epsilon=eps)(X)
+
+
+def least_squares(A, b, cond=0.1):
     """ Returns a Least squares solution of Ax = b
 
     Args:
-        A (npt.NDArray[np.float32]): Input array for the least squres problem
+        A (npt.NDArray[np.float32]): Input array for the least squares problem
         b (npt.NDArray[np.float32]): Output array for the least squares problem
+        cond : cut-off ratio for singular values of a
     Returns:
         npt.NDArray[np.float32]: Least squares solution of Ax = b
     """
     # TODO: Implement using scipy.linalg.lstsq 
-    # Hint: Don't forget that lstsq also returns other parameters such as residuals, rank, singular values etc. 
-    pass 
-    
+    # Hint: Don't forget that lstsq also returns other parameters such as residuals, rank, singular values etc.
+    # Solve the least squares problem using scipy.linalg.lstsq
+    A_augment = np.column_stack([A, np.ones(A.shape)])
+    x, residuals, rank, s = lstsq(A_augment, b, cond=cond)
 
+    # Return the solution
+    return x
+
+
+def transform(X, coefficients):
+    X_augment = np.column_stack([X, np.ones(X.shape)])
+    return coefficients @ X_augment.T
