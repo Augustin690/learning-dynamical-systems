27472c96addb6a9f00ae380d1a4514f8c90e9973
Augustin <augustin.curinier@laposte.net>
2024-06-20 17:32:54 +0200

    change of radial_basis_function implementation to pass automated test

--- src/part1/utils.py
+++ src/part1/utils.py
@@ -8,6 +8,7 @@ from scipy.integrate import solve_ivp
 
 # TODO: Define all the functions (in addition to the ones below) required to complete tasks 1-3 in this python file.
 
+
 # Evaluation of a single Radial Basis Function (RBF)
 def radial_basis_function(x, x_c, eps):
     """
@@ -17,57 +18,18 @@ def radial_basis_function(x, x_c, eps):
     - x: Data points
     - x_c: Centers of the basis functions
     - eps: Epsilon parameter(bandwidth)
-    
+
     Returns:
     - Radial basis function values evaluated at data points x
     """
     # Hint: Use cdist from scipy.spatial.distanc
-    phi_x = np.exp(-(cdist(x_c, x, 'sqeuclid') / eps ** 2))
+    phi_x = np.exp(-(cdist(x_c, x, "sqeuclid") / eps**2))
 
-    return phi_x
-
-
-def diameter(X):
-    """
-    Compute the diameter of dataset X, that is max(dist(Xi,Xj)) for all Xi, Xj in the dataset. Used to compute epsilon parameter.
-
-    Args:
-    - X: dataset
-    
-    Returns:
-    - Maximum of pairwise distances within the dataset
-    """
-
-    pairwise_distances = cdist(X, X, 'euclidean')
-
-    return np.max(pairwise_distances)
+    return phi_x.T
 
 
-def approx_non_linear_function(X, L, ratio):
-    x_c = np.linspace(-3.5, 4.5, L).reshape((L, 1))
-    X = X.reshape((X.shape[0],1))
-    eps = ratio * diameter(X)
-    phi_x = radial_basis_function(X, x_c, eps)
-    # return transform(X, least_squares(phi_x, Y))
-    return phi_x
-
-
-def built_int_interpolator(X, Y, eps):
-    # X_augment = np.column_stack([X, np.ones(X.shape)])
-
-    return RBFInterpolator(X, Y, kernel='gaussian', epsilon=eps)(X)
-
-def approx_non_linear_field(X, centers, ratio):
-    #x_c = np.linspace(-3.5, 4.5, L).reshape((L, 1))
-    #X = X.reshape((X.shape[0],1))
-    eps = ratio * diameter(X)
-    phi_x = radial_basis_function(X, centers, eps)
-    # return transform(X, least_squares(phi_x, Y))
-    return phi_x
-
-
-def least_squares(A, b, cond=0.001):
-    """ Returns a Least squares solution of Ax = b
+def least_squares(A, b, cond=0.01):
+    """Returns a Least squares solution of Ax = b
 
     Args:
         A (npt.NDArray[np.float32]): Input array for the least squares problem
@@ -76,7 +38,7 @@ def least_squares(A, b, cond=0.001):
     Returns:
         npt.NDArray[np.float32]: Least squares solution of Ax = b
     """
-    # TODO: Implement using scipy.linalg.lstsq 
+    # TODO: Implement using scipy.linalg.lstsq
     # Hint: Don't forget that lstsq also returns other parameters such as residuals, rank, singular values etc.
     # Solve the least squares problem using scipy.linalg.lstsq
 
@@ -84,45 +46,54 @@ def least_squares(A, b, cond=0.001):
     return lstsq(A, b, cond=cond)
 
 
-def lstq_residuals(A, b, cond=0.001):
-    x, residuals, rank, s = lstsq(A, b, cond=cond)
-    return residuals
+def linear_fit(X,Y):
+       """Fits linear model between X (input data) and Y (dependent dat)
+
+    Args:
+        X: Input array 
+        Y: Dependatn array, f(X)
+    Returns:
+        npt.NDArray[np.float64]: Least squares coefficients
+    """
+       return least_squares(X,Y)[0]
 
 
-def transform(X, coefficients):
-    #_augment = np.column_stack([X, np.ones(X.shape)])
-    # return coefficients @ X_augment.T
-    #print('updated')
-    return np.dot(X,coefficients)
+
+def transform(X, C):
+    """Applies changes to input data X according to the coefficient of the newly fit linear model
+
+    Args:
+        X: Input array to transform
+        C: Coefficient array solution to least squares problem CX = Y
+    Returns:
+        npdt.NDArray[np.float64]: Transformation of X according to linear model
+    """
+    return np.dot(X,C)
 
 
-def x1_estim(callable_fun, x_0, T):
-    for i in range(len(x_0)):
-        solve = solve_ivp(callable_fun, [0, T], x_0[i, :], t_eval=[T])
-        if i == 0:
-            x1_estim = solve.y.T
-        else:
-            x1_estim = np.row_stack((x1_estim, solve.y.T))
-        # print(solve.y.T)
-    return x1_estim
+def linear_fit_transform(X,Y): 
+    """Fits linear model on X,Y then apply transformation on X using coefficients of newly fit model
 
-
-def trajectory(callable_fun, x_0,T, t_eval):
-    solve = solve_ivp(callable_fun, [0, T], x_0, t_eval=t_eval)
-
-    return solve.y.T
+    Args:
+        X: Input array 
+        Y: Dependent array, f(X)
+    Returns:
+        npt.NDArray[np.float64]: transformation of X according to linear model
+    """
+    coef = linear_fit(X,Y)
+    return transform(X,coef)
 
 def mean_squared_error(y_true, y_pred):
     """
-      Compute the mean squared Euclidean distance between two matrices.
+    Compute the mean squared Euclidean distance between two matrices.
 
-      Args:
-          y_true (np.ndarray): The matrix of actual values.
-          y_pred (np.ndarray): The matrix of predicted values.
+    Args:
+        y_true (np.ndarray): The matrix of actual values.
+        y_pred (np.ndarray): The matrix of predicted values.
 
-      Returns:
-          float: The mean squared Euclidean distance.
-      """
+    Returns:
+        float: The mean squared Euclidean distance.
+    """
     if y_true.shape != y_pred.shape:
         raise ValueError("The shape of y_true and y_pred must be the same.")
 
@@ -136,3 +107,68 @@ def mean_squared_error(y_true, y_pred):
     mean_squared_distance = np.mean(row_squared_distances)
 
     return mean_squared_distance
+
+
+def diameter(X):
+    """
+    Compute the diameter of dataset X, that is max(dist(Xi,Xj)) for all Xi, Xj in the dataset. Used to compute epsilon parameter.
+
+    Args:
+    - X: dataset
+
+    Returns:
+    - Maximum of pairwise distances within the dataset
+    """
+
+    pairwise_distances = cdist(X, X, "euclidean")
+
+    return np.max(pairwise_distances)
+
+
+def approx_non_linear_function(X, L, ratio):
+    x_c = (np.max(X[:,0]) - np.min(X[:,0]))*np.random.random_sample((L,1)) + np.max(X[:,0])
+    #X = X.reshape((X.shape[0], 1))
+    eps = ratio * diameter(X)
+    phi_x = radial_basis_function(X, x_c, eps)
+    # return transform(X, least_squares(phi_x, Y))
+    return phi_x
+
+
+def built_int_interpolator(X, Y, eps):
+    # X_augment = np.column_stack([X, np.ones(X.shape)])
+
+    return RBFInterpolator(X, Y, kernel="gaussian", epsilon=eps)(X)
+
+
+def approx_non_linear_field(X, centers, ratio):
+    # x_c = np.linspace(-3.5, 4.5, L).reshape((L, 1))
+    # X = X.reshape((X.shape[0],1))
+    eps = ratio * diameter(X)
+    phi_x = radial_basis_function(X, centers, eps)
+    # return transform(X, least_squares(phi_x, Y))
+    return phi_x
+
+
+def lstq_residuals(A, b, cond=0.001):
+    x, residuals, rank, s = lstsq(A, b, cond=cond)
+    return residuals
+
+
+
+def x1_estim(callable_fun, x_0, T):
+    for i in range(len(x_0)):
+        solve = solve_ivp(callable_fun, [0, T], x_0[i, :], t_eval=[T])
+        if i == 0:
+            x1_estim = solve.y.T
+        else:
+            x1_estim = np.row_stack((x1_estim, solve.y.T))
+        # print(solve.y.T)
+    return x1_estim
+
+
+def trajectory(callable_fun, x_0, T, t_eval):
+    solve = solve_ivp(callable_fun, [0, T], x_0, t_eval=t_eval)
+
+    return solve.y.T
+
+
--- src/part2/utils.py
+++ src/part2/utils.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 # TODO: Define all the functions (in addition to the ones below) required to complete tasks 4-5 in this python file.
 
+
 def time_delay(data, col: int, delta_t: int, out_dim: int):
     """
     Create time delay coordinates from the data.
@@ -13,30 +14,37 @@ def time_delay(data, col: int, delta_t: int, out_dim: int):
     :param delta_t: No. of time-steps used for delayed measurement.
     :param out_dim: The target output dimension (2 or 3) for the output coordinates.
     :return: A numpy array of time-delay coordinates.
-    
-    Hint: 
-    For out_dim = 2, 
+
+    Hint:
+    For out_dim = 2,
         - shape of delayed coordinates = (np.shape(data)[0] - delta_t, 2)
         - Column 1: data with rows starting from 0 to -delta_t, col = col
         - Column 2: data with rows starting from delta_t to the last row, col = col
         - Stack the 2 columns to form the delayed coordinates
 
-    For out_dim = 3, 
+    For out_dim = 3,
         - Column 1: data with rows starting from 0 to -2*delta_t, col = col
         - Column 2: data with rows starting from delta_t to -delta_t, col = col
         - Column 3: data with rows starting from 2*delta_t till the end, col = col
-        - Stack the 3 columns to form the delayed coordinates    
+        - Stack the 3 columns to form the delayed coordinates
     """
     # TODO: Implement this function!
     # column = data[:,col]
     if out_dim == 2:
         # delayed_embed = np.empty((np.shape(data)[0] - delta_t), out_dim)
-        delayed_embed = np.column_stack((data[0:-delta_t, col], data[delta_t:np.shape(data)[0], col]))
+        delayed_embed = np.column_stack(
+            (data[0:-delta_t, col], data[delta_t : np.shape(data)[0], col])
+        )
         return delayed_embed
 
     elif out_dim == 3:
-        delayed_embed = np.column_stack((data[0:-2 * delta_t, col], data[delta_t: -delta_t, col],
-                                         data[2 * delta_t:np.shape(data)[0], col]))
+        delayed_embed = np.column_stack(
+            (
+                data[0 : -2 * delta_t, col],
+                data[delta_t:-delta_t, col],
+                data[2 * delta_t : np.shape(data)[0], col],
+            )
+        )
 
         return delayed_embed
 
@@ -45,13 +53,13 @@ def time_delay(data, col: int, delta_t: int, out_dim: int):
 def create_windows(data, num_delays, num_areas):
     windows = []
     for start in range(len(data) - num_delays):
-        window = data[start:start + num_delays + 1, :num_areas]
+        window = data[start : start + num_delays + 1, :num_areas]
         windows.append(window.flatten())
     return np.array(windows)
 
 
 def center_data(data: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
-    """ Center data by subtracting the mean of the data.
+    """Center data by subtracting the mean of the data.
 
     Args:
         data (npt.NDArray[np.float64]): Data matrix.
@@ -64,9 +72,12 @@ def center_data(data: np.ndarray[np.float64]) -> np.ndarray[np.float64]:
     return data - mean
 
 
-def compute_svd(data: np.ndarray[np.float64]) -> tuple[
-    np.ndarray[np.float64], np.ndarray[np.float64], np.ndarray[np.float64]]:
-    """ Compute (reduced) SVD of the data matrix. Set (full_matrices=False).
+def compute_svd(
+    data: np.ndarray[np.float64],
+) -> tuple[
+    np.ndarray[np.float64], np.ndarray[np.float64], np.ndarray[np.float64]
+]:
+    """Compute (reduced) SVD of the data matrix. Set (full_matrices=False).
 
     Args:
         data (npt.NDArray[np.float]): data matrix.
@@ -80,7 +91,7 @@ def compute_svd(data: np.ndarray[np.float64]) -> tuple[
     return np.linalg.svd(data, full_matrices=False)
 
 
-def compute_energy(S: np.ndarray[np.float64], c:int = 1) -> np.float64:
+def compute_energy(S: np.ndarray[np.float64], c: int = 1) -> np.float64:
     """
     Percentage of total “energy” (explained variance) of (only) the i-th principal component of singular value on the diagonal of the matrix S.
         Note that it is NOT a sum of first 'c' components!
@@ -95,7 +106,8 @@ def compute_energy(S: np.ndarray[np.float64], c:int = 1) -> np.float64:
     # TODO: Implement method.
     total_energy = np.sum(S**2)
     energy = S**2 / total_energy
-    return energy[c-1] * 100
+    return energy[c - 1] * 100
+
 
 ### FUNCTIONS FROM PART 1 USED TO APPROXIMATE LORENZ VECTOR FIELD
 
@@ -114,14 +126,14 @@ def radial_basis_function(x, x_c, eps):
     - Radial basis function values evaluated at data points x
     """
     # Hint: Use cdist from scipy.spatial.distanc
-    phi_x = np.exp(-(cdist(x_c, x, 'sqeuclid') / eps ** 2))
+    phi_x = np.exp(-(cdist(x_c, x, "sqeuclid") / eps**2))
 
     return phi_x
 
 
 def diameter(X):
     # return np.max(X) - np.min(X)
-    pairwise_distances = cdist(X, X, 'euclidean')
+    pairwise_distances = cdist(X, X, "euclidean")
     # Find the maximum distance
     return np.max(pairwise_distances)
 
@@ -136,7 +148,7 @@ def approx_non_linear_field(X, centers, ratio):
 
 
 def least_squares(A, b, cond=0.001):
-    """ Returns a Least squares solution of Ax = b
+    """Returns a Least squares solution of Ax = b
 
     Args:
         A (npt.NDArray[np.float32]): Input array for the least squares problem
@@ -157,15 +169,15 @@ def least_squares(A, b, cond=0.001):
 
 def mean_squared_error(y_true, y_pred):
     """
-      Compute the mean squared Euclidean distance between two matrices.
+    Compute the mean squared Euclidean distance between two matrices.
 
-      Args:
-          y_true (np.ndarray): The matrix of actual values.
-          y_pred (np.ndarray): The matrix of predicted values.
+    Args:
+        y_true (np.ndarray): The matrix of actual values.
+        y_pred (np.ndarray): The matrix of predicted values.
 
-      Returns:
-          float: The mean squared Euclidean distance.
-      """
+    Returns:
+        float: The mean squared Euclidean distance.
+    """
     if y_true.shape != y_pred.shape:
         raise ValueError("The shape of y_true and y_pred must be the same.")
 
