5bc9c6121291ecd77e73187a1966337a935aafa1
Augustin <augustin.curinier@laposte.net>
2024-06-21 03:04:25 +0200

    Refactoring and cleaning code, Task 2 & 3

--- src/part1/utils.py
+++ src/part1/utils.py
@@ -15,15 +15,15 @@ def radial_basis_function(x, x_c, eps):
     Compute radial basis function values as defined in the worksheet in section 1.2 (equation (7))
 
     Args:
-    - x: Data points
-    - x_c: Centers of the basis functions
-    - eps: Epsilon parameter(bandwidth)
+        x: Data points
+        x_c: Centers of the basis functions
+        eps: Epsilon parameter(bandwidth)
 
     Returns:
-    - Radial basis function values evaluated at data points x
+        Radial basis function values evaluated at data points x
     """
     # Hint: Use cdist from scipy.spatial.distanc
-    phi_x = np.exp(-(cdist(x_c, x, "sqeuclid") / eps**2))
+    phi_x = np.exp(-(cdist(x_c, x, "sqeuclid") / eps ** 2))
 
     return phi_x.T
 
@@ -47,11 +47,11 @@ def least_squares(A, b, cond=0.01):
 
 
 def linear_fit(X, Y):
-    """Fits linear model between X (input data) and Y (dependent dat)
+    """Fits linear model between X (input data) and Y (dependent data)
 
     Args:
         X: Input array
-        Y: Dependatn array, f(X)
+        Y: Dependant array, f(X)
     Returns:
         npt.NDArray[np.float64]: Least squares coefficients
     """
@@ -65,7 +65,7 @@ def transform(X, C):
         X: Input array to transform
         C: Coefficient array solution to least squares problem CX = Y
     Returns:
-        npdt.NDArray[np.float64]: Transformation of X according to linear model
+        npdt.NDArray[np.float64]: Transformation of X according to linear model == approximation of Y
     """
     return np.dot(X, C)
 
@@ -83,13 +83,22 @@ def linear_fit_transform(X, Y):
     return transform(X, coef)
 
 
-def non_linear_fit_transform(X,Y,centers,ratio):
+def non_linear_fit_transform(X, Y, x_c, ratio):
+    """Compute radial basis functions of X, then fits a linear model between phi(X) and Y, and apply linear
+    transformation to phi in order to compute non-linear approximation of Y
+
+    Args:
+        X: Input array
+        Y: Dependent array, f(X)
+        x_c: radial basis function centers
+        ratio: float used to compute epsilon
+    Returns:
+        npt.NDArray[np.float64]: transformation of X according to linear model
+    """
 
     epsilon = ratio * diameter(X)
-
-    phi_x = radial_basis_function(X, centers, epsilon)
-
-    return linear_fit_transform(phi_x,Y)
+    phi_x = radial_basis_function(X, x_c, epsilon)
+    return linear_fit_transform(phi_x, Y)
 
 
 def mean_squared_error(y_true, y_pred):
@@ -123,10 +132,10 @@ def diameter(X):
     Compute the diameter of dataset X, that is max(dist(Xi,Xj)) for all Xi, Xj in the dataset. Used to compute epsilon parameter.
 
     Args:
-    - X: dataset
+        X: dataset
 
     Returns:
-    - Maximum of pairwise distances within the dataset
+        Maximum of pairwise distances within the dataset
     """
 
     pairwise_distances = cdist(X, X, "euclidean")
@@ -134,8 +143,33 @@ def diameter(X):
     return np.max(pairwise_distances)
 
 
+### USEFUL FOR TASK 2
+
+def x1_estim(f, x_0, delta_T):
+    """
+      Compute estimate of future state x1 given x0, delta_T and f, derivative of flow operator to be integrated over
+      time
+
+      Args:
+        x_0: array representing system state at t=0
+        f: callable function, derivative of flow operator
+        delta_T: span of time separating state x0 and state x1 to be approximated
+
+      Returns:
+        Approximation of future state x1 after delta_T
+      """
+    for i in range(len(x_0)):
+        solve = solve_ivp(f, [0, delta_T], x_0[i, :], t_eval=[delta_T])
+        if i == 0:
+            x1_approx = solve.y.T
+        else:
+            x1_approx = np.row_stack((x1_approx, solve.y.T))
+        # print(solve.y.T)
+    return x1_approx
+
+
 def PHI(X, L, ratio):
-        """
+    """
     Compute radial basis function values, given L and epsilon.
 
     Args:
@@ -144,12 +178,12 @@ def PHI(X, L, ratio):
     Returns:s
     - Radial basis function values
     """
-        x_c = np.linspace(-4.5,4.5,L).reshape((L,1))
-        # X = X.reshape((X.shape[0], 1))
-        eps = ratio * diameter(X)
-        phi_x = radial_basis_function(X, x_c, eps)
-        # return transform(X, least_squares(phi_x, Y))
-        return phi_x
+    x_c = np.linspace(-4.5, 4.5, L).reshape((L, 1))
+    # X = X.reshape((X.shape[0], 1))
+    eps = ratio * diameter(X)
+    phi_x = radial_basis_function(X, x_c, eps)
+    # return transform(X, least_squares(phi_x, Y))
+    return phi_x
 
 
 def built_int_interpolator(X, Y, eps):
@@ -170,17 +204,6 @@ def approx_non_linear_field(X, centers, ratio):
 def lstq_residuals(A, b, cond=0.001):
     x, residuals, rank, s = lstsq(A, b, cond=cond)
     return residuals
-
-
-def x1_estim(callable_fun, x_0, T):
-    for i in range(len(x_0)):
-        solve = solve_ivp(callable_fun, [0, T], x_0[i, :], t_eval=[T])
-        if i == 0:
-            x1_estim = solve.y.T
-        else:
-            x1_estim = np.row_stack((x1_estim, solve.y.T))
-        # print(solve.y.T)
-    return x1_estim
 
 
 def trajectory(callable_fun, x_0, T, t_eval):
